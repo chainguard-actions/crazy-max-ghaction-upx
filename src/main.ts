d958dc67e28e84cba63a70ae6c7a7c1ce15bd1fe
actionchain <actionchain@chainguard.dev>
2026-06-16 00:11:56 +0000

    Update crazy-max/ghaction-upx@v4.0.0

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import * as os from 'os';
+import * as path from 'path';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
+import * as context from './context.js';
+import * as installer from './installer.js';
+
+async function run(): Promise<void> {
+  try {
+    if (os.platform() == 'darwin') {
+      core.setFailed('Not supported on darwin platform');
+      return;
+    }
+
+    const inputs: context.Inputs = await context.getInputs();
+    const upx = await installer.getUPX(inputs.version);
+
+    if (inputs.installOnly) {
+      const dir = path.dirname(upx);
+      core.addPath(dir);
+      core.debug(`Added ${dir} to PATH`);
+      return;
+    }
+
+    const files: string[] = context.resolvePaths(inputs.files);
+    if (files.length == 0) {
+      core.warning(`No files were found. Please check the 'files' input.`);
+      return;
+    }
+
+    await context.asyncForEach(files, async filepath => {
+      core.startGroup(`Compressing ${filepath}...`);
+      await exec.exec(`${upx} ${inputs.args} ${filepath}`);
+      core.endGroup();
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
